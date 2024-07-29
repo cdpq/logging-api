54bc46ae74315a5eb86c3ed5ef4009ced1096efa
Sylvain Gagnon <sgagnon@cdpqinc.ca>
2024-07-29 16:31:41 -0400

    Version clean

--- logging_api.py
+++ logging_api.py
@@ -4,8 +4,6 @@ import sys
 app = Flask(__name__)
 
 #This is in the main app, normaly. I removed the whole datastruct.
-projets = {}
-elements = {}
 
 running_port = sys.argv[1] if len(sys.argv) > 1 else 5000
 output_file = sys.argv[2] if len(sys.argv) > 2 else "log.txt"
