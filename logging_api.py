4107447118973cf81ed99043bec3b1a66a7d56cb
mbelanger2 <mbelanger@cdpq.ca>
2024-07-31 09:09:00 -0400

    Vérification et creation du répertoire de log Vérification du type de post Vérification code de retour de l'accès fichier Ajout de la creation du fichier de log si il n'existe pas. ajout du parametre host dans le app.run pour permettre d'écouter sur toutes les adresses IP.

--- logging_api.py
+++ logging_api.py
@@ -1,21 +1,36 @@
 from flask import Flask, jsonify, request
 import sys
+import os
 
+BASE_LOGFOLDER = "log_files"
 app = Flask(__name__)
-
-#This is in the main app, normaly. I removed the whole datastruct.
 
 running_port = sys.argv[1] if len(sys.argv) > 1 else 5000
 output_file = sys.argv[2] if len(sys.argv) > 2 else "log.txt"
-output_target = "log_files/" + output_file
+output_target = os.path.join(BASE_LOGFOLDER ,  output_file)
+if not os.path.exists(BASE_LOGFOLDER):
+    os.makedirs(BASE_LOGFOLDER)
 
 @app.route('/', methods=['POST'])
 def racine_post():
-    # with open(output_target, "a", encoding="utf-8") as out_file:
-    with open(output_target, "a", encoding="utf-8") as out_file:
-            out_file.write(str(request.form) + "\n")
+    if request.is_json:
+        message = str(request.json)
+    elif request.data :
+        message = request.data.decode("utf-8")
+    elif request.form :
+         message = str(request.form)
+    elif request.args :
+         message = str(request.args)
+    else :
+        message = str(request)
+    try :
+        with open(output_target, "a+", encoding="utf-8") as out_file:
+                out_file.write(message + "\n")
+    except Exception as e: 
+         return f"Erreur à l'écriture du log {e}"                
+    
     return "Information logged"
 
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False, port=running_port)
+    app.run(host="0.0.0.0", port=running_port)
